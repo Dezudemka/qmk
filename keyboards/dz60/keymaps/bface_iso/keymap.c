2524f096c2165ef1cf546f89252f7aae99ed6a22
dirk <dirk.oecinghaus@gmail.com>
2018-10-14 14:41:06 +0200

    merge with upstream

--- keyboards/dz60/keymaps/bface_iso/keymap.c
+++ keyboards/dz60/keymaps/bface_iso/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * | LCtrl |  LGUI  |  LAlt  |          Space           |  RAlt  |  RGUI  | Layer_2 |  RCtrl |
 * `-----------------------------------------------------------------------------------------'
 */
-	[0] = KEYMAP(
+	LAYOUT_directional(
 		KC_ESC,   KC_1,     KC_2,     KC_3,   KC_4,   KC_5,   KC_6,     KC_7,     KC_8,   KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_NO,  KC_BSPC,
 		KC_TAB,   KC_Q,     KC_W,     KC_E,   KC_R,   KC_T,   KC_Y,     KC_U,     KC_I,   KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
 		MO(1),    KC_A,     KC_S,     KC_D,   KC_F,   KC_G,   KC_H,     KC_J,     KC_K,   KC_L,     KC_SCLN,  KC_QUOT, KC_ENT,
@@ -30,14 +30,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// 	KC_LSFT, KC_NO, KC_NO, KC_NO, BL_DEC, BL_TOGG, BL_INC, BL_STEP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
   // 	KC_LCTL, KC_LGUI, KC_LGUI, KC_NO, KC_NO, KC_NO, KC_RALT, KC_RGUI, KC_NO, KC_NO, KC_RCTL),
 
-  [1] = KEYMAP(
+  LAYOUT_directional(
 		KC_GRV,       KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_NO,  KC_DEL,
 		KC_CAPSLOCK,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGUP,  KC_UP,    KC_PGDN,  KC_PSCR,  KC_INS,   KC_PAUS,  KC_TRNS,
 		KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 		KC_LSFT,      KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_END,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,
     KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NO,    KC_TRNS,  KC_TRNS),
 
-  [2] = KEYMAP(
+  LAYOUT_directional(
     KC_TRNS,  KC_MAIL, KC_CALC, KC_MYCM,  KC_WHOM,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_WAKE,  KC_SLEP,  KC_NO,  KC_TRNS,
     KC_TRNS,  KC_VOLU, KC_MSTP, KC_MPLY,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
     KC_TRNS,  KC_VOLD, KC_MPRV, KC_MNXT,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NO,    KC_TRNS,  KC_TRNS),
 
 
-  [3] = KEYMAP(
+  LAYOUT_directional(
     KC_NO,    RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_SAI,  RGB_VAI,  KC_NO,    BL_TOGG,  BL_INC,   KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
     KC_NO,    KC_NO,    KC_NO,    RGB_HUD,  RGB_SAD,  RGB_VAD,  KC_NO,    BL_STEP,  BL_DEC,   KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,
